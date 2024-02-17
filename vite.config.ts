c1b12661c3773f6ad813085aeaff89e6b8a23eb6
sykim0181 <soyeon364@naver.com>
2024-02-17 10:13:06 +0900

    no message

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __dirname = path.dirname(__filename);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/commerce/",
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
