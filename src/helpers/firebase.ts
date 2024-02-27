1e227372cd63e73f6aec33118f35c918f3853679
sykim0181 <soyeon364@naver.com>
2024-02-27 20:14:59 +0900

    MODIFY: 환경변수명 변경

--- src/helpers/firebase.ts
+++ src/helpers/firebase.ts
@@ -10,13 +10,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_apiKey,
-  authDomain: import.meta.env.VITE_authDomain,
-  projectId: import.meta.env.VITE_projectId,
-  storageBucket: import.meta.env.VITE_storageBucket,
-  messagingSenderId: import.meta.env.VITE_messagingSenderId,
-  appId: import.meta.env.VITE_APP_appId,
-  measurementId: import.meta.env.VITE_measurementId
+  apiKey: import.meta.env.VITE_firebase_apiKey,
+  authDomain: import.meta.env.VITE_firebase_authDomain,
+  projectId: import.meta.env.VITE_firebase_projectId,
+  storageBucket: import.meta.env.VITE_firebase_storageBucket,
+  messagingSenderId: import.meta.env.VITE_firebase_messagingSenderId,
+  appId: import.meta.env.VITE_firebase_appId,
+  measurementId: import.meta.env.VITE_firebase_measurementId
 };
 
 // Initialize Firebase
