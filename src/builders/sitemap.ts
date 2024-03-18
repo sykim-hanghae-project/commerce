80fd5a8c07280a316ecaa5feb74b86f3c4b48ab6
sykim0181 <soyeon364@naver.com>
2024-03-18 12:43:31 +0900

    MODIFY: sitemap url 변경

--- src/builders/sitemap.ts
+++ src/builders/sitemap.ts
@@ -2,7 +2,7 @@ import GenerateSitemap from "react-router-sitemap-maker";
 import MyRoutes from "../routes/routes";
 
 const sitemapData = await GenerateSitemap(MyRoutes(), {
-	baseUrl: "https://sykim-hanghae-project.github.io/commerce",
+	baseUrl: "https://commerce-seven-chi-53.vercel.app",
 	hashrouting: true,
 	changeFrequency: "monthly"
 });
